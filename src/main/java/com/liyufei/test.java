badf15bc19fa22f014cc1ae5e2a3d22db1b93a55
李宇飞 <873315036@qq.com>
2019-09-20 10:45:15 +0800

    ddd

--- src/main/java/com/liyufei/test.java
+++ src/main/java/com/liyufei/test.java
@@ -6,6 +6,8 @@ package com.liyufei;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("8888");
+		
+		System.out.println("7777");
 		System.out.println("9999");
 	}
 }
