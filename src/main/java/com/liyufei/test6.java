dd537d1278f662538881d7857cf414283e78d1f5
李宇飞 <873315036@qq.com>
2019-09-20 12:25:38 +0800

    ddd

--- src/main/java/com/liyufei/test6.java
+++ src/main/java/com/liyufei/test6.java
@@ -6,6 +6,7 @@ package com.liyufei;
 public class test6 {
 	public static void main(String[] args) {
 		System.out.println("9999999");
+		System.out.println("9999999");
 	}
 }
 
--- src/main/java/com/liyufei/test888.java
+++ src/main/java/com/liyufei/test888.java
@@ -6,6 +6,7 @@ package com.liyufei;
 public class test888 {
 	public static void main(String[] args) {
 		System.out.println("hhhhh");
+		System.out.println("54332");
 	}
 }
 
