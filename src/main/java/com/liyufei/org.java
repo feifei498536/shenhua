622ec83b8352ffcb1b43beb14211fd8079a8dc4b
李宇飞 <873315036@qq.com>
2019-09-20 14:37:16 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: 李宇飞 <873315036@qq.com>

--- src/main/java/com/liyufei/org.java
+++ src/main/java/com/liyufei/org.java
@@ -3,12 +3,10 @@ package com.liyufei;
 * @author 作者 李宇飞
 * @since 创建时间：2019年9月20日 上午10:33:46 
 */
-public class test {
-	public static void main1(String[] args) {
-        System.out.println("fangfa1");
-	}
+public class org {
 
-    public static void main2(String[] args) {
+    public static void get(String[] args) {
+
         System.out.println("方法2");
     }
 }
